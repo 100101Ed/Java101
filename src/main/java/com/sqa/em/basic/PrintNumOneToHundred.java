121e7ebb0de38ab2cfeda8e25e8d8991f6304732
100101Ed <edm100101@hotmail.com>
2016-01-23 17:17:49 -0800

    Add app that would print 1 to 100

---
+++ src/main/java/com/sqa/em/basic/PrintNumOneToHundred.java
@@ -0,0 +1,31 @@
+/**
+ * File Name: PrintOneToHundred.java<br>
+ * Mora, Eduardo<br>
+ * Java Boot Camp Exercise<br>
+ * Instructor: Jean-francois Nepton<br>
+ * Created: Jan 23, 2016
+ */
+package com.sqa.em.basic;
+
+/**
+ * PrintOneToHundred //Will print a counting from 1 to 100
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ *
+ * @author Mora, Eduardo
+ * @version 1.0.0
+ * @since 1.0
+ */
+public class PrintNumOneToHundred {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		for (int i = 1; i < 101; i++) {
+			System.out.print(i + " ");
+		}
+	}
+}
